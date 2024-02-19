89e87bb16ed2416aa33d69800e2d2e8daa4e6528
shreyasg197 <shreyas.gaikwad@fortanix.com>
2024-02-19 09:02:21 +0530

    first changes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -194,7 +194,8 @@ local default_plugins = {
   -- file managing , picker etc
   {
     "nvim-tree/nvim-tree.lua",
-    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    -- cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    lazy = false,
     init = function()
       require("core.utils").load_mappings "nvimtree"
     end,
